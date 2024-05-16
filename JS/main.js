14e24a7dcf5554e0839018cb991a9757c0c2da2a
Baldezin <valdirjuniorprincipal@gmail.com>
2024-05-16 14:29:46 -0300

    3º Commit

--- JS/main.js
+++ JS/main.js
@@ -7,3 +7,14 @@ function colocarImagem() {
     img1.style.display = "block";
     img2.style.display = "block";
 }
+
+function mudarImagem() {
+    if (img1.src = "./imagens/greatball.png") {
+        img1.src = "./imagens/greataberta.png";
+    }
+    
+    if (img2.src = "./imagens/moonball.png") {
+        img2.src = "./imagens/moonaberta.png";
+    }
+
+}
