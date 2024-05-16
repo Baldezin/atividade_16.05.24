f9c4b9e3dd5923327be756e47cea0a6ac96f0adb
Baldezin <valdirjuniorprincipal@gmail.com>
2024-05-16 14:12:27 -0300

    2º Commit

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+var img1 = document.getElementById("imagem1");
+var img2 = document.getElementById("imagem2");
+img1.style.display = "none";
+img2.style.display = "none";
+
+function colocarImagem() {
+    img1.style.display = "block";
+    img2.style.display = "block";
+}
